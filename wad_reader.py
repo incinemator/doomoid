2dda424237971184fc1b1312bcd8a67d6dd3c455
Incinemator <giannakzorz@gmail.com>
2024-03-19 20:59:43 +0100

    Update

--- wad_reader.py
+++ wad_reader.py
@@ -1,1 +1,12 @@
-# Open and close .wad file
+# Read the wad file ine binary mode
+import struct
+
+class WADReader:
+    def __init__(self, wad_path):
+        self.wad_file = open(wad_path, 'rb')
+
+    def read_bytes(self, offset, num_bytes, byte_format):
+        self.wad_file.seek(offset)
+
+    def close(self):
+        self.wad_file.close()
